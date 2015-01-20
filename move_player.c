6ea79ad4c56554612e359898b0e5726102777d31
Jonathan CHICHEPORTICHE <jchichep@e3r7p6.42.fr>
2015-01-20 12:09:28 +0100

    board

---
+++ move_player.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   move_player.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jchichep <jchichep@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/01/20 11:41:10 by jchichep          #+#    #+#             */
+/*   Updated: 2015/01/20 11:59:27 by jchichep         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void move_player(int joueur, int direction, int *pos, char tab[21][61])
+{
+	int y;
+	if (joueur == 1)
+		y = 2;
+	else
+		y = 57;
+	if (direction == 1 && *pos < 16)
+	{
+		tab[*pos][y] = ' ';
+		tab[*pos + 3][y] = '|';
+		++(*pos);
+	}
+	if (direction == -1 && *pos > 1)
+	{
+		tab[*pos + 2][y] = ' ';
+		tab[*pos - 1][y] = '|';
+		--(*pos);
+	}
+}
